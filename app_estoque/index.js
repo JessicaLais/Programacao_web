9b6449f400f062a40e63e1c33deaf80e2a2d9e73
JessicaLais <142358374+JessicaLais@users.noreply.github.com>
2024-04-12 18:19:20 -0300

    Update index.js

--- app_estoque/index.js
+++ app_estoque/index.js
@@ -1,26 +1,27 @@
-const express = require ('express');
+const express = require('express');
 const estoque = require('./src/estoque');
 const app = express();
 
-app.get("/adicionar/:id/:nome/:qtd", function(req,res){
+app.get("/adicionar/:id/:nome/:qtd", function (req, res) {
     let id = req.params.id;
     let nome = req.params.nome;
     let qtd = req.params.qtd;
     let p = estoque.criarProduto(id, nome, qtd);
+    estoque.adicionarProduto(p);
     res.send(p);
 });
 
-app.get('/listar', function(req,res){
-    res.send(estoque.listarProdutos());
+app.get('/listar', function (req, res) {
+    res.send(estoque.listarProduto());
 });
 
-app.get('/remover/:id', function(req,res){
+app.get('/remover/:id', function (req, res) {
     let id = req.params.id;
-    estoque.removerProdutos(id);
+    estoque.removerProduto(id);
     res.send(id);
 });
 
 const PORT = 8080;
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log('app rodando na porta' + PORT)
 });
